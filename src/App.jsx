dd3e541bab23eecce49da5598b2a4f1740d2dde4
Jhenkar MS <jhenkar5nov3@gmail.com>
2026-02-14 12:08:32 +0530

    Initial hero setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <div>
-      Portfolio App
+      {/* <Navbar /> */}
+      <Hero />
     </div>
   )
 }
